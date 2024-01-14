a7f6a95854c95a85333d896a00348a779fa1369f
sjhong98 <sjhong98@icloud.com>
2024-01-15 01:59:19 +0900

    debug:

--- src/pages/zustand-tetris/blocks.tsx
+++ src/pages/zustand-tetris/blocks.tsx
@@ -1,5 +1,7 @@
 import { blockType } from ".";
 
+
+
 export const IBlock = [5, 15, 25, 35];
 export const SBlock = [5, 6, 14, 15];
 export const ZBlock = [4, 5, 15, 16];
--- src/pages/zustand-tetris/board.tsx
+++ src/pages/zustand-tetris/board.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { blockType } from ".";
 import { IBlock, ZBlock, SBlock, JBlock, LBlock, OBlock, TBlock } from "./blocks";
+import _ from 'lodash';
 
 export default function Board() {
     // zustand state 처리할것
@@ -14,8 +15,11 @@ export default function Board() {
     const blockStyle = "bg-gray-800 rounded-sm center";
     const blockFilledStyle = "bg-white rounded-sm center"
 
+    const randomBlockSelection = [IBlock, ZBlock, SBlock, LBlock, JBlock, OBlock, TBlock];
+
     const blockMove = () => {
-        let temp = [...blocks];
+        // 얕은 복사가 문제였음. spread 사용해도 얕은 복사되어 같은 주소 참조함.
+        let temp = _.cloneDeep(blocks);
         if(sortedBlock[0]+10<200 && !temp[sortedBlock[0]+10].active) {
             temp[sortedBlock[0]].active = false;
             temp[sortedBlock[0]+10].active = true;
@@ -30,20 +34,24 @@ export default function Board() {
                         temp[sortedBlock[3]+10].active = true;
                         setBlocks(temp);
                         let tempArr:number[] = [];
+                        console.log("moved");
                         for(let i=0; i<4; i++) {
                             let tempNum:number = sortedBlock[i];
                             tempArr.push(tempNum+10);
                             setSortedBlock(tempArr);
                         }
-                    } else {console.log("exit at 3"); setBlockChange(true);}
-                } else {console.log("exit at 2"); setBlockChange(true);}
-            } else {console.log("exit at 1"); setBlockChange(true);}
-        } else {console.log("exit at 0"); setBlockChange(true);}
+                    } else {console.log("exit at 3\n\n\n\n"); setBlockChange(true);}
+                } else {console.log("exit at 2\n\n\n\n"); setBlockChange(true);}
+            } else {console.log("exit at 1\n\n\n\n"); console.log("before blocks : ", blocks); setBlockChange(true);}     // 여기서 탈출하는 애들이 문제. 한번 변한 temp가 반영이 되어버림
+        } else {console.log("exit at 0\n\n\n\n"); setBlockChange(true);}
     }
 
     useEffect(() => {
         if(blockChange) {
-            let tempSorted:number[] = OBlock;
+            console.log("blocks : ", blocks)
+            console.log("changing\n\n\n\n");
+            // let tempSorted:number[] = randomBlockSelection[Math.floor(Math.random() * randomBlockSelection.length)];
+            let tempSorted:number[] = LBlock;
             tempSorted.sort((a, b) => b-a);
             let tempStack = [...stack];
             setSortedBlock(tempSorted);
@@ -67,7 +75,7 @@ export default function Board() {
         if(blocks.length !== 0) {
             intervalId = setTimeout(() => {
                 blockMove();
-            }, 500)
+            }, 200)
         }
         // setInterval이 중첩되어 동작하지 않도록 하기 위해서는 clearInterval 사용
         // useEffect가 실행될 때마다 새로운 setInterval이 생성됨
